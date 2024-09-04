79cb5ff750bfa7938b2c06df6bf4a04b09c20d1f
PavelOS10 <osiov25@gmail.com>
2024-09-04 15:17:55 +0300

    models registry

---
+++ Prog/models/registry.py
@@ -0,0 +1,17 @@
+from data.db_handler import DBHandler
+
+class Registry:
+    def init(self):
+        self.db = DBHandler()
+
+    def add_animal(self, animal):
+        self.db.add_animal(animal)
+
+    def get_animal(self, name):
+        return self.db.get_animal_by_name(name)
+
+    def teach_command(self, name, command):
+        animal = self.get_animal(name)
+        if animal:
+            new_commands = animal['команды'].split(', ') + [command]
+            self.db.update_animal_commands(animal['id'], ', '.join(new_commands))
